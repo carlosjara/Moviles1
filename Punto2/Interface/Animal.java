4f03139bfdacd797eec455f48abd898d156d8d39
carlosjara <ceja0919@gmail.com>
2016-02-14 16:25:47 -0500

    Creando carpeta del punto 2

---
+++ Punto2/Interface/Animal.java
@@ -0,0 +1,6 @@
+package punto2.tarea;
+
+public interface Animal {
+	public void eat();
+    public void travel();
+}
---
+++ Punto2/Interface/TestAbstraction2.java
@@ -0,0 +1,15 @@
+package punto2.tarea;
+
+class TestAbstraction2{
+    public static void main(String args[]){
+        Mammal m1 = new Mammal();
+        Birds b1 = new Birds();
+        
+        b1.eat();
+        m1.travel();
+        m1.eat();
+        b1.travel();
+        System.out.println(m1.numOfLegs());  
+    }
+}
+
